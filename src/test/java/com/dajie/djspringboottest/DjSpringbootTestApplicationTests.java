c3d3f82cb81b64ed1b1751696b80dc4af40a67c6
unknown <yukun.zhu@zhu-yu-kun.im20-group.com>
2018-08-21 16:53:18 +0800

    test

---
+++ src/test/java/com/dajie/djspringboottest/DjSpringbootTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dajie.djspringboottest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DjSpringbootTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
