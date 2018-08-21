a8536872f068d17f5743672361fdceb3d3e571fa
unknown <yukun.zhu@zhu-yu-kun.im20-group.com>
2018-08-21 17:23:11 +0800

    tijiao

--- src/main/java/com/dajie/djspringboottest/controller/SampleGitController.java
+++ src/main/java/com/dajie/djspringboottest/controller/SampleGitController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-public class SampleController {
-    private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
+public class SampleGitController {
+    private static final Logger logger = LoggerFactory.getLogger(SampleGitController.class);
 
     @RequestMapping("/")
     @ResponseBody
     public String home(HttpServletRequest request) {
         logger.info("------Hello World!-------- {}",request.getServletPath());
-        return "Hello World!Hello World!Hello World!Hello World!";
+        return "Hello World!Hello World!Hello World!Hello World!Hello World!Hello World";
     }
 }
